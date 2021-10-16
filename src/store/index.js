8257de246e91e0565439544188a0bb842fac3a36
Ivan Escalante <ivan-enrique.escalante-cornejo@ibm.com>
2021-10-16 14:39:50 -0400

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counter";
+import authenticationReducer from "./auth";
+
+const store = configureStore({
+  reducer: { counter: counterReducer, auth: authenticationReducer },
+});
+
+export default store;
